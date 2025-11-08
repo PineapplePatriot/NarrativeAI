02da985a6c249384e0114045c4d029a4e13b138b
Andrii <bilyjandrij4@gmail.com>
2025-11-08 14:49:53 +0200

    Fix profile html, format user

--- narrative/users/static/users/js/profile.js
+++ narrative/users/static/users/js/profile.js
@@ -1,11 +1,11 @@
 const photoInput = document.getElementById("id_photo");
 const photoPreview = document.getElementById("photo-preview");
 
-photoInput.addEventListener("change", function() {
+photoInput.addEventListener("change", function () {
     const file = this.files[0];
     if (file) {
         const reader = new FileReader();
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             photoPreview.src = e.target.result;
         };
         reader.readAsDataURL(file);
